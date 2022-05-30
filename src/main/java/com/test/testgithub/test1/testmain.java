e3d74c2539b2b72463d47da4c51e221082112a9a
shenYue <da_niu_de@163.com>
2022-05-30 23:36:47 +0800

    first commit

---
+++ src/main/java/com/test/testgithub/test1/testmain.java
@@ -0,0 +1,14 @@
+package com.test.testgithub.test1;
+
+/**
+ * @packageName : com.test.testgithub.test1
+ * @ClassName : testmain
+ * @Author : shenYue
+ * @Date: 2022/5/30 23:31
+ * @Description : 1
+ */
+public class testmain {
+    public static void main(String[] args) {
+        System.out.println("hello world!!");
+    }
+}
